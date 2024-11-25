58ddf68d38ff224fa2cdf1b58973b860794247db
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 05:56:05 +0900

    feat: CoinController 추가

---
+++ src/main/java/vendingmachine/controller/CoinController.java
@@ -0,0 +1,14 @@
+package vendingmachine.controller;
+
+import static vendingmachine.view.InputView.input;
+import static vendingmachine.view.OutputView.printHavingMoneyMessage;
+
+import vendingmachine.model.Coin;
+
+public class CoinController {
+
+    public void initMoney() {
+        printHavingMoneyMessage();
+
+    }
+}
