70d42b8c5639bc8d398e5bd1c05d32fe62ac8ac9
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 20:20:23 +0900

    feat: main에서 controller 인스턴스 초기화 및 보유 동전 설정

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,10 @@
 package vendingmachine;
 
+import vendingmachine.controller.CoinController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CoinController coinController = new CoinController();
+        coinController.initMoney();
     }
 }
