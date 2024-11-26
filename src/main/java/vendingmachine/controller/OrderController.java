89b32e233507134cb763b0b266de43568d2f7448
woojin cheon <qpqpqp0102@naver.com>
2024-11-27 02:10:13 +0900

    feat: 일정 조건 하에서 주문 계속 반복되는 로직 구현

--- src/main/java/vendingmachine/controller/OrderController.java
+++ src/main/java/vendingmachine/controller/OrderController.java
@@ -1,13 +1,30 @@
 package vendingmachine.controller;
 
+import static vendingmachine.controller.ProductController.minPrice;
+import static vendingmachine.controller.ProductController.validateEmptyStock;
+import static vendingmachine.model.Products.getProductByName;
 import static vendingmachine.view.InputView.input;
+import static vendingmachine.view.OutputView.printPurchasingItemMessage;
+import static vendingmachine.view.OutputView.printPurchasingMoney;
 
+import vendingmachine.model.Product;
 import vendingmachine.model.Products;
 
 public class OrderController {
 
-    public void makeOrder(Products products, int money) {
-        String purchasingItemName = input();
-//        Product purchasingItem =
+    public void makeOrder(Products products, int purchasingMoney) {
+        while (validateEmptyStock(products) && purchasingMoney > minPrice(products)) {
+            printPurchasingMoney(purchasingMoney);
+            //투입금액(남은금액) 출력
+            printPurchasingItemMessage();
+            String purchasingItemName = input();
+            Product purchasingItem = getProductByName(purchasingItemName);
+            purchasingMoney -= purchasingItem.getPrice();
+        }
+        giveChange();
+    }
+
+    public void giveChange() {
+        
     }
 }
