9c838dfdda60ee9eae89223d96ef26e483c2cc4c
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 21:54:23 +0900

    feat: int 변환 메소드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,6 +4,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
+    public static int inputInteger() {
+        return Integer.parseInt(input());
+    }
+
     public static String input() {
         return readLine();
     }
