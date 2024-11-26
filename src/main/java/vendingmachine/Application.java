e9ab128c13fff43a2ba4155ef84b2b81adb66886
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 21:53:49 +0900

    feat: 프로그램 흐름 조정하는 코드 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,10 +1,21 @@
 package vendingmachine;
 
 import vendingmachine.controller.CoinController;
+import vendingmachine.controller.MoneyController;
+import vendingmachine.controller.OrderController;
+import vendingmachine.controller.ProductController;
+import vendingmachine.model.Products;
 
 public class Application {
     public static void main(String[] args) {
         CoinController coinController = new CoinController();
-        coinController.initMoney();
+        ProductController productController = new ProductController();
+        MoneyController moneyController = new MoneyController();
+        OrderController orderController = new OrderController();
+
+        int[] coins = coinController.initMoney();
+        Products products = productController.initStock();
+        int money = moneyController.receiveMoney();
+        orderController.makeOrder(products, money);
     }
 }
