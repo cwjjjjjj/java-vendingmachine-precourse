bb6a2330bcdea8064bce14486f2bf6aa88ef6ef1
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 21:55:47 +0900

    feat: 주문 관련 controller

---
+++ src/main/java/vendingmachine/controller/OrderController.java
@@ -0,0 +1,13 @@
+package vendingmachine.controller;
+
+import static vendingmachine.view.InputView.input;
+
+import vendingmachine.model.Products;
+
+public class OrderController {
+
+    public void makeOrder(Products products, int money) {
+        String purchasingItemName = input();
+//        Product purchasingItem =
+    }
+}
