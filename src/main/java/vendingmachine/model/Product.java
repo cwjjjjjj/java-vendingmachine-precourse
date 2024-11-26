60ffbb192e940fad056ddf9b8db6290ac425c832
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 21:55:05 +0900

    feat: 재고 상품의 데이터 담는 클래스

---
+++ src/main/java/vendingmachine/model/Product.java
@@ -0,0 +1,30 @@
+package vendingmachine.model;
+
+public class Product {
+
+    private String name;
+    private int price;
+    private int stock;
+
+    public Product(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void reduceStock() {
+        this.stock -= 1;
+    }
+}
