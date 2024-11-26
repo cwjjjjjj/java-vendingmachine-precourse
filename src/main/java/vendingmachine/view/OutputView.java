046517418da177dc787b4da8dc7c1274c023ac04
woojin cheon <qpqpqp0102@naver.com>
2024-11-26 21:54:35 +0900

    feat: 출력 메소드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -6,23 +6,26 @@ public class OutputView {
 
     private static final String HAVING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String HAVING_COIN_MESSAGE = "\n자판기가 보유한 동전";
-    private static final String STOCK_MESSAGE = "\n상품명과 가격, 수량을 입력해 주세요.";
+    private static final String ORDER_MESSAGE = "\n상품명과 가격, 수량을 입력해 주세요.";
     private static final String PURCHASING_MONEY_MESSAGE = "\n투입 금액을 입력해 주세요.";
     private static final String printForm = "%d원 -  %d개";
-
 
     public static void printHavingMoneyMessage() {
         System.out.println(HAVING_MONEY_MESSAGE);
     }
 
-    public static void printHavingCoinMessage() {
-        System.out.println(HAVING_COIN_MESSAGE);
-    }
-
     public static void printCoins(int[] coins) {
+        System.out.println(HAVING_COIN_MESSAGE);
         for (int i = 0; i < coins.length; i++) {
             System.out.println(String.format(printForm, Coin.values()[i].getAmount(), coins[i]));
         }
+    }
 
+    public static void printOrderMessage() {
+        System.out.println(ORDER_MESSAGE);
+    }
+
+    public static void printPurchasingMoneyMessage() {
+        System.out.println(PURCHASING_MONEY_MESSAGE);
     }
 }
